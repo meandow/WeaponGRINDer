8d03ed621cb43b96306cebf4aced11dcd7ddbf70
Henrik Svensson <henriksvensson101@gmail.com>
2019-08-09 13:22:31 +0200

    Add message when durability is low

--- WeaponGRINDer/WeaponGRINDer.lua
+++ WeaponGRINDer/WeaponGRINDer.lua
@@ -7,15 +7,40 @@
 local function GrindWeapon()
 	weaponId = 16
 	canRepair = CanMerchantRepair()
-	current, maximum = GetInventoryItemDurability(weaponId);
-	needsRepair = (current/maximum) < 0.8
-    if canRepair and needsRepair then
+    if canRepair then
         ShowRepairCursor()
 		PickupInventoryItem(weaponId)
     end
 end
 
+local function CheckWeapon()
+	weaponId = 16
+	canRepair = CanMerchantRepair()
+	current, maximum = GetInventoryItemDurability(weaponId);
+	durability = current/maximum
+    if durability == 0 then
+		message('Your weapon is broken.')
+	elseif durability < 0.12 then
+	    message('Your weapon will break on next death.')
+	elseif durability < 0.37 then
+        message('Your weapon will break on next spirit release.')
+    end
+end
+
+
 -- event frame
 local grinder = CreateFrame("Frame")
 grinder:RegisterEvent("MERCHANT_SHOW")
 grinder:SetScript("OnEvent", GrindWeapon)
+
+local checkDurabilityAlive = CreateFrame("Frame")
+checkDurabilityAlive:RegisterEvent("PLAYER_ALIVE")
+checkDurabilityAlive:SetScript("OnEvent", CheckWeapon)
+
+local checkDurabilityGhost = CreateFrame("Frame")
+checkDurabilityGhost:RegisterEvent("PLAYER_UNGHOST")
+checkDurabilityGhost:SetScript("OnEvent", CheckWeapon)
+
+local checkDurabilityZone = CreateFrame("Frame")
+checkDurabilityZone:RegisterEvent("ZONE_CHANGED")
+checkDurabilityZone:SetScript("OnEvent", CheckWeapon)
